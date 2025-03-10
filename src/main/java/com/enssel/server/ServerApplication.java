8a6b9cc2cb6af7bb221c55edb00ab5a8c285a00d
yujongyeob <230233@naver.com>
2025-03-10 17:27:02 +0900

    server enable(websocket)

--- src/main/java/com/enssel/server/ServerApplication.java
+++ src/main/java/com/enssel/server/ServerApplication.java
@@ -2,9 +2,14 @@ package com.enssel.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
+	@Bean
+	public WebSocketHandler myWebSocketHandler() {
+		return new WebSocketHandler();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
--- src/main/java/com/enssel/server/WebSocketHandler.java
+++ src/main/java/com/enssel/server/WebSocketHandler.java
@@ -1,5 +1,7 @@
 package com.enssel.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -9,30 +11,36 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
     private static final ConcurrentHashMap<String, WebSocketSession> CLIENTS = new ConcurrentHashMap<String, WebSocketSession>();
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("Connected to " + session.getRemoteAddress());
         CLIENTS.put(session.getId(), session);
+        broadcastMessage("TEST");
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("Disconnected from " + session.getRemoteAddress());
         CLIENTS.remove(session.getId());
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String id = session.getId();  //메시지를 보낸 아이디
-        CLIENTS.entrySet().forEach( arg->{
-            if(!arg.getKey().equals(id)) {  //같은 아이디가 아니면 메시지를 전달합니다.
-                try {
-                    arg.getValue().sendMessage(message);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        log.info("Recevied text message: " + message.getPayload());
+
+    }
+    public void broadcastMessage(Object data) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonMessage = objectMapper.writeValueAsString(data);
+        for (WebSocketSession session : CLIENTS.values()) {
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(jsonMessage));
             }
-        });
+        }
     }
 }
