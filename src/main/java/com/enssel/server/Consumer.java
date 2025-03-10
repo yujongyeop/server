b417f6cf9ec11615e0124f1c0b66d4f95fac8d45
jongyeob <230233@naver.com>
2025-03-10 15:57:19 +0900

    websocket

--- src/main/java/com/enssel/server/Consumer.java
+++ src/main/java/com/enssel/server/Consumer.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class Consumer {
     private static final Logger log = LoggerFactory.getLogger(Consumer.class);
     private final ObjectMapper objectMapper = new ObjectMapper();
